68d4c969b306c8e7d7465abc700eaa1ff764b371
jeffersonanjos <20211101110051@ifrn.local>
2025-09-12 16:09:18 -0300

    Entrega 01

--- database.py
+++ database.py
@@ -7,10 +7,8 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
 
-
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
 
 def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
