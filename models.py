4d5f41a07b5f0337c19604a07fddd9d125e35c38
jeffersonanjos <20211101110051@ifrn.local>
2025-09-12 16:08:30 -0300

    Entrega 01

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, JSON
 from typing import List, Optional
 from datetime import datetime
 
+#class 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True, unique=True)
