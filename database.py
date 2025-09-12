4fcbfeabc44f6a57262ee19d8ddc7a284a898bac
Jefferson Santos <jefferson@email.com>
2025-09-12 16:13:50 -0300

    Entrega 01

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlmodel import SQLModel, create_engine, Session
 from typing import Generator
 import os
 
-
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
